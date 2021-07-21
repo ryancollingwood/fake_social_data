0c5c9ba6efbe62260e4c6397cbefce369b38bb41
Ryan Collingwood <ryancollingwood@gmail.com>
2021-07-21 18:30:40 +1000

    Write out data into partitions

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from datetime import datetime, timezone
+from pathlib import Path
 import getpass
 import random
 import json
@@ -39,7 +40,7 @@ def get_random_items(from_list, max_items: int, chance_is_empty: int = 0):
     return set(result)
 
 
-def generate_meta_data(fake: Faker, number_of_records: int, random_seed: int):
+def generate_meta_data(fake: Faker, number_of_records: int, random_seed: int, start_index: int, end_index: int):
     meta_data = dict()
 
     generated_at = datetime.now()
@@ -52,8 +53,14 @@ def generate_meta_data(fake: Faker, number_of_records: int, random_seed: int):
         "timestamp": generated_at.astimezone(timezone.utc).isoformat()
 
     }
-    meta_data["user"] = getpass.getuser()
-    meta_data["number_of_records"] = number_of_records
+    meta_data["records"] = {
+        "index": {
+            "start": start_index,
+            "end": end_index,
+        },
+        "number_of_records": number_of_records
+    }
+    meta_data["user"] = getpass.getuser()    
     meta_data["random_seed"] = random_seed
 
     return meta_data
@@ -141,7 +148,7 @@ def generate_reciprocal_friends(people):
     return people
 
 
-def execute(number_of_people, random_seed, output_path):
+def execute(number_of_people, random_seed, number_of_partitions, output_path):
     # initialise faker and set our seeds
     fake = Faker()
 
@@ -149,19 +156,37 @@ def execute(number_of_people, random_seed, output_path):
     random.seed(random_seed)
 
     # make some fake data    
-    people = generate_people(fake, number_of_people)
-    meta_data = generate_meta_data(fake, len(people), random_seed)
+    people = generate_people(fake, number_of_people)    
     people = generate_reciprocal_friends(people)
+    
+    # assumes we've got even division
+    records_per_partition = int(number_of_people / number_of_partitions)
 
-    data = {
-        "meta_data": meta_data,
-        "data": people,
-        }
+    # confirm we can access or create the output path
+    try:
+        Path(output_path).mkdir(parents=True, exist_ok=True)
+    except Exception as e:
+        print(f"Could not create or access the path: {output_path}")
+        raise e
 
-    with open(output_path, 'w') as outfile:
-        json.dump(data, outfile)
+    for i in range(number_of_partitions):
+        start_index = i * records_per_partition
+        end_index = start_index + records_per_partition
 
-    print(json.dumps(meta_data, indent=4))
+        people_in_partition = people[start_index:end_index]
+
+        meta_data = generate_meta_data(fake, len(people_in_partition), random_seed, start_index, end_index - 1)
+
+        data = {
+            "meta_data": meta_data,
+            "data": people_in_partition,
+            }
+
+        with open(f"{output_path}/{i+1}.json", 'w') as outfile:
+            json.dump(data, outfile)
+
+        print
+        print(json.dumps(meta_data, indent=4))
 
 
 if __name__ == '__main__':
@@ -170,10 +195,13 @@ if __name__ == '__main__':
     RANDOM_SEED = 42
 
     # how many fake people do we wanna generate?
-    NUMBER_OF_PEOPLE = 200
+    NUMBER_OF_PEOPLE = 2000
+
+    # how many chunks of data do we want to write out
+    NUMBER_PARTITIONS = 10
 
     # where to write out the data
-    OUTPUT_PATH = "data.json"
+    OUTPUT_PATH = "output"
 
     args_parser = argparse.ArgumentParser(description='Generate fake people data')
 
@@ -191,14 +219,28 @@ if __name__ == '__main__':
                        nargs = '?',
                        default = RANDOM_SEED)
 
+    args_parser.add_argument('Partitions',
+                       metavar = 'partitions',
+                       type = int,
+                       help = 'Number of partitions to write the data out into',
+                       nargs = '?',
+                       default = NUMBER_PARTITIONS)
 
     args_parser.add_argument('Output',
                        metavar = 'output',
                        type = str,
-                       help = 'Where to write out the generated data',
+                       help = 'Directory to write out the partitions of generated data',
                        nargs = '?',
                        default = OUTPUT_PATH)
 
-    args = args_parser.parse_args()    
+    args = args_parser.parse_args()
 
-    execute(args.N, args.Seed, args.Output)
+    try:
+        assert( args.N % args.Partitions == 0)
+    except AssertionError as e:
+        print("Please specify a number of records and partitions that will be evenly distributed")
+        print(f"Number of records ({args.N}) does not evenly divide by number of partitions ({args.Partitions})")
+        print("e.g. 100 into 10 paritions")
+        raise argparse.ArgumentTypeError("Number of records does not evenly divide by number of partitions")
+
+    execute(args.N, args.Seed, args.Partitions, args.Output)
